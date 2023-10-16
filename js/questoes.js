281cf7afda650072110b244fd6a7c86034e18bd7
Lara Poleto <110049663+LaraPoleto@users.noreply.github.com>
2023-10-15 23:15:49 -0300

    Initial commit

---
+++ js/questoes.js
@@ -0,0 +1,359 @@
+const questoes = [
+    {
+        questao: 'Questão 1 -',
+
+        comando: '(UFSC/2019-2) Ao longo da história, até em livros de medicina, o clitóris – do grego kleitoris, “pequeno monte” – é pouco mencionado. Porém, na França, onde a educação sexual é obrigatória nas escolas desde a primeira infância, estudantes de todas as faixas etárias têm a oportunidade de conhecer de perto o órgão erétil, o principal órgão do prazer feminino e de anatomia similar à do pênis, através de um modelo tridimensional. O mapeamento recente de suas estruturas através de ressonância magnética foi o que permitiu descobrir que o órgão mede de 9 a 11 cm e que é constituído pela glande, bulbos, cruras e corpo, sendo a primeira estrutura a única parte externa do clitóris. Sobre sistema genital feminino, é correto afirmar que:',
+
+        alternativas: [{
+            letra: '01',
+            alternativa: 'O clitóris é um órgão erétil feminino, com anatomia similar à do pênis, e assim como este é responsável pela eliminação da urina.',
+            classificacao: 'errada'
+        },
+        {
+            letra: '02',
+            alternativa: 'A ocorrência da cistite (infecção da bexiga) é mais comum em mulheres do que em homens devido à localização do clitóris, que possui a maior extensão situada internamente ao sistema urogenital, diferentemente do pênis.',
+            classificacao: 'errada'
+        },
+        {
+            letra: '04',
+            alternativa: 'O clitóris é densamente vascularizado e possui muitos receptores sensoriais.',
+            classificacao: 'correta'
+        },
+        {
+            letra: '08',
+            alternativa: 'O hormônio gonadotrofina coriônica é responsável por estimular a ereção do clitóris.',
+            classificacao: 'errada'
+        },
+        {
+            letra: '16',
+            alternativa: 'Assim como o clitóris, a abertura externa da uretra se localiza no interior da vagina.',
+            classificacao: 'errada'
+        },],
+
+        explicacao:
+            '01) Falsa. A eliminação da urina ocorre por meio da abertura (óstio) da uretra. \ \n \ \n 02) Falsa. A maior ocorrência da cistite em mulheres, quando comparado aos homens, deve-se à menor extensão da uretra \ \n \ \n 04) Verdadeira. \ \n \ \n 08) Falsa. A estimulação do clitóris é por ação nervosa (o clitóris tem cerca de 8 mil terminações nervosas). \ \n \ \n 16) Falsa. A abertura externa da uretra localiza-se sobre a abertura vaginal, sendo integrante da vulva (pudendo feminino). \ \n \ \n  Soma: 4',
+    },
+    {
+        questao: 'Questão 2 -',
+
+        comando: ' O sistema reprodutor feminino além dos órgãos externos, é constituído pelo útero, os ovário e as tubas uterinas. Dentre os órgãos internos existe um meio de ligação. Assinale o órgão que desempenha este papel:',
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'vagina.',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'b',
+            alternativa: 'pudendo feminino.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'sistema urinário feminino.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'd',
+            alternativa: 'hímen.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'e',
+            alternativa: 'vestíbulo vaginal',
+            classificacao: 'errada'
+        },],
+
+        explicacao:
+            'Letra A, pois a vagina é o meio de ligação dos órgãos externos para os internos.',
+    },
+    {
+        questao: 'Questão 3 -',
+
+        comando: 'Sobre as partes do útero, marque a alternativa incorreta:',
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'A porção dilatada do útero é chamada de corpo do útero.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'O colo do útero é a parte mais baixa do útero.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'O fundo do útero apresenta forma de cúpula.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'd',
+            alternativa: 'Os ovários penetram na parte superior do útero.',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'e',
+            alternativa: 'O fundo do útero está localizado entre as duas tubas uterinas.',
+            classificacao: 'errada'
+        },],
+
+        explicacao:
+            'Letra D. Os ovários não penetram no útero. O útero conecta-se à sua parte superior às tubas.',
+    },
+    {
+        questao: 'Questão 4 -',
+
+        comando: ' O principal vírus relacionado ao câncer do colo do útero é o:',
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'HIV',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'Influenza',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'HPV',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'd',
+            alternativa: 'Poliovírus',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'e',
+            alternativa: 'HSV',
+            classificacao: 'errada'
+        },],
+
+        explicacao:
+            'Letra C. O HPV, Vírus do Papiloma Humano, está relacionado com o desenvolvimento do câncer do colo do útero e é transmitido, principalmente, por via sexual.',
+    },
+    {
+        questao: 'Questão 5 -',
+
+        comando: ' A fecundação é um evento em que há a união do espermatozóide com o ovócito secundário. Esse processo ocorre em qual órgão do sistema genital feminino?',
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'Vagina',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'Útero',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'Tuba uterina',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'd',
+            alternativa: 'Ovário',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'e',
+            alternativa: 'Vulva',
+            classificacao: 'errada'
+        },],
+
+        explicacao:
+            'Letra C. Essa formação ocorre no interior das trompas, e o óvulo fecundado em seguida se encaminha na direção do útero.',
+    },
+    {
+
+        questao: 'Questão 6 -',
+
+        comando: 'A gravidez ectópica ocorre quando:',
+
+
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'a nidação do embrião ocorre no útero.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'o óvulo e o endométrio sofrem descamação.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'a implantação do óvulo fertilizado ocorre fora da cavidade uterina.',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'd',
+            alternativa: 'o embrião se desenvolve a partir de um óvulo sem ocorrer a fecundação.',
+            classificacao: 'errada'
+        },
+        ],
+        explicacao:
+            'Letra C. Gravidez ectópica é aquela em que a implantação do blastocisto ocorre fora da cavidade uterina como, por exemplo, na tuba uterina.',
+    },
+    {
+        questao: 'Questão 7 -',
+
+        comando: 'Muitas mulheres apresentam um distúrbio no ovário que se caracteriza pelo aparecimento de cistos nesse órgão. Normalmente mulheres com esse problema possuem ciclos menstruais irregulares e dificuldade para engravidar. Esse problema no ovário é chamado de:',
+
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'Dismenorreia',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'Câncer do colo do útero',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'Mioma',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'd',
+            alternativa: 'TPM',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'e',
+            alternativa: 'Síndrome do ovário policístico',
+            classificacao: 'correta'
+        },
+        ],
+        explicacao:
+            'Letra E.Distúrbio hormonal que causa um aumento no tamanho dos ovários, com pequenos cistos na parte externa deles.',
+    },
+    {
+        questao: 'Questão 8 -',
+
+        comando:'(UERJ- alterada) Mulher estéril voltou a produzir óvulos após receber um transplante de ovário congelado nos Estados Unidos (0 Globo, 24/09/99). \ \n \ \n No procedimento médico-cirúrgico acima, o tecido ovariano transplantado foi induzido por hormônios a produzir óvulos. Isso foi possível porque a função ovariana é estimulada pelos seguintes hormônios secretados pela hipófise:',
+
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'estrogênio e progesterona.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: 'estrogênio e hormônio Luteinizante',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: ' folículo estimulante e progesterona.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'd',
+            alternativa: 'folículo estimulante e hormônio Luteinizante',
+            classificacao: 'errada'
+        },
+        ],
+        explicacao:
+            'Letra D. Os hormônios secretados  pela hipófise são o LH (Hormônio Luiteinizante) e o FSH (Hormônio Folículo Estimulante).',
+    },
+    {
+        questao: 'Questão 9 -',
+
+        comando:'A menstruação é um processo em que ocorre o desprendimento do endométrio e sua eliminação, junto ao sangue, através da vagina. Esse processo ocorre quando os níveis:',
+       
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'de LH aumentam e FSH diminuem.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: ' de LH e FSH aumentam.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'de progesterona e estrógeno diminuem.',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'd',
+            alternativa: 'de progesterona e estrógeno aumentam.',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'e',
+            alternativa: 'de progesterona aumentam e o estrógeno diminui.',
+            classificacao: 'errada'
+        },
+        ],
+        explicacao:
+            'Letra C. Assim que a liberação do ovócito secundário (que acontece no período fértil), O LH realiza uma transformação  do folículo ovariano em que o mesmo se tornará um corpo amarelo. Ele irá produzxir estrógeno e progesterona, que vão inibir a produção do LH e FSH. Com essa redução, o corpo amarelo vai parar de produzir estrógeno e progesterona. A queda de ambos hormônios fará com que aconteca a menstruação.',
+    },
+    {
+        questao: 'Questão 10 -',
+
+        comando:'O Hímen está localizado:',
+       
+        alternativas: [{
+            letra: 'a',
+            alternativa: 'No Endométrio',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'b',
+            alternativa: ' No Colo do Útero',
+            classificacao: 'errada'
+        },
+        {
+            letra: 'c',
+            alternativa: 'No Vestíbulo Vaginal',
+            classificacao: 'correta'
+        },
+        {
+            letra: 'd',
+            alternativa: 'Nas Tubas Uterinas',
+            classificacao: 'errada'
+        },
+        ],
+        explicacao:
+            'Letra C. O hímen é uma membrana presente no vestíbulo vaginal.',
+    }
+]
+
+
+const ques = document.getElementById('questao');
+let index = 0;
+for (let questao of questoes) {
+    let listaAlternativas = questao.alternativas;
+    ques.innerHTML +=
+        `<form action="">
+        <div class="nome">${questao.questao}</div>
+        <div class="comando">${questao.comando}</div>
+    `;
+    for (let alternativa of listaAlternativas) {
+        ques.innerHTML += `
+        <div class="alt">
+        <input type="radio" id="${alternativa.letra}" 
+        name="questao${index}" 
+        class="${alternativa.classificacao}" />
+        <label for="${alternativa.letra}" class="classLabel">
+        ${alternativa.letra}) ${alternativa.alternativa}</label>
+        </div>
+        `;
+    }
+
+    ques.innerHTML += `
+    <div class="divBtn">
+    <button class="btn1" onclick="corrigir('questao${index}')">Responder</button>
+    <button type="button" class="btn1" data-bs-toggle="modal" onclick="exibirModal(${index})">Ver Explicação</button>
+    </div>
+
+    </form>`;
+    index++;
+
+}
+
